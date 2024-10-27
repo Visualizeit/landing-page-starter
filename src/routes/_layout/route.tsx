fd441f260c7ff043bfc36f36ef4752af096026b8
Visualizeit <17688172112@163.com>
2024-10-27 19:51:02 +0800

    Refactor AppLayout component and update background styling

--- src/routes/_layout/route.tsx
+++ src/routes/_layout/route.tsx
@@ -4,7 +4,11 @@ import Header from '@/components/Header'
 
 const AppLayout = () => {
 	return (
-		<AppShell withBorder={false} header={{ height: 56 }}>
+		<AppShell
+			className="bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
+			withBorder={false}
+			header={{ height: 56 }}
+		>
 			<AppShell.Header className="bg-transparent backdrop-blur-lg">
 				<Header />
 			</AppShell.Header>
