290850eaa863dee11fbf26902789acc6d71da55a
Visualizeit <17688172112@163.com>
2024-10-29 20:38:00 +0800

    Refactor Footer component to update layout and add About section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,62 +1,59 @@
-import {
-	Group,
-	Stack,
-	ThemeIcon,
-	Title,
-	Text,
-	Anchor,
-	Container,
-} from '@mantine/core'
+import { Group, Stack, ThemeIcon, Title, Text, Anchor } from '@mantine/core'
 import { IconSparkles } from '@tabler/icons-react'
 import { Link } from '@tanstack/react-router'
+import Section from './Section'
 
 const Footer = () => {
 	return (
 		<footer className="w-full">
-			<Container size="xl">
-				<Stack gap="sm" py="xl">
-					<Group justify="space-between" align="center">
-						<Stack gap="sm">
-							<Group gap="xs">
-								<ThemeIcon variant="transparent" size="lg">
-									<IconSparkles className="size-full" />
-								</ThemeIcon>
-								<Title order={3}>Surpass</Title>
-							</Group>
-							<Text size="sm">
-								© {new Date().getFullYear()} Surpass Studio. All Rights
-								Reserved.
-							</Text>
-						</Stack>
-						<Group align="start" gap="xl">
-							<Stack gap="xs">
-								<Title order={6}>Product</Title>
-								<Anchor component={Link} c="gray" size="sm">
-									All Features
-								</Anchor>
-								<Anchor component={Link} c="gray" size="sm">
-									Release Notes
-								</Anchor>
-								<Anchor component={Link} c="gray" size="sm">
-									Documentation
-								</Anchor>
-							</Stack>
-							<Stack gap="xs">
-								<Title order={6}>Support</Title>
-								<Anchor component={Link} c="gray" size="sm">
-									FAQ
-								</Anchor>
-								<Anchor component={Link} c="gray" size="sm">
-									Privacy Policy
-								</Anchor>
-								<Anchor component={Link} c="gray" size="sm">
-									Terms of Service
-								</Anchor>
-							</Stack>
+			<Section>
+				<Group justify="space-evenly" align="start">
+					<Stack gap="sm">
+						<Group gap="xs">
+							<ThemeIcon variant="transparent" size="lg">
+								<IconSparkles className="size-full" />
+							</ThemeIcon>
+							<Title order={3}>Surpass</Title>
 						</Group>
-					</Group>
-				</Stack>
-			</Container>
+						<Text c="gray" size="sm">
+							© {new Date().getFullYear()} Surpass Studio.
+						</Text>
+					</Stack>
+					<Stack gap="sm">
+						<Title order={6}>Product</Title>
+						<Anchor component={Link} c="gray" size="sm">
+							All Features
+						</Anchor>
+						<Anchor component={Link} c="gray" size="sm">
+							Release Notes
+						</Anchor>
+						<Anchor component={Link} c="gray" size="sm">
+							Documentation
+						</Anchor>
+					</Stack>
+					<Stack gap="sm">
+						<Title order={6}>Support</Title>
+						<Anchor component={Link} c="gray" size="sm">
+							FAQ
+						</Anchor>
+						<Anchor component={Link} c="gray" size="sm">
+							Privacy Policy
+						</Anchor>
+						<Anchor component={Link} c="gray" size="sm">
+							Terms of Service
+						</Anchor>
+					</Stack>
+					<Stack gap="sm">
+						<Title order={6}>About</Title>
+						<Anchor component={Link} c="gray" size="sm">
+							About Us
+						</Anchor>
+						<Anchor component={Link} c="gray" size="sm">
+							Contact Us
+						</Anchor>
+					</Stack>
+				</Group>
+			</Section>
 		</footer>
 	)
 }
