f8153cf611d2a29c39a60ce3c3fd2aaf17cf2ed4
Visualizeit <17688172112@163.com>
2024-10-29 21:30:36 +0800

    Refactor Testimonials component to update social media handles and improve UI

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -11,52 +11,70 @@ import Section from './Section'
 
 interface Testimonial {
 	name: string
-	title: string
+	social: string
 	quote: string
 }
 
 const testimonials1: Testimonial[] = [
 	{
-		name: 'John Doe',
-		title: 'CEO, Company Inc.',
+		name: 'Pomni',
+		social: '@pomni',
 		quote:
-			'Your product is exactly what we needed. It has been a pleasure working with your team.',
+			"The mobile app is fantastic. I can manage our operations on-the-go, which has greatly improved our team's responsiveness.",
 	},
 	{
-		name: 'Jane Smith',
-		title: 'CTO, Tech Solutions',
+		name: 'Caine',
+		social: '@caine',
 		quote:
-			'I have been using your product for a while now and I am very impressed with the results.',
+			'Security is paramount in our industry, and this platform has exceeded our expectations.',
+	},
+	{
+		name: 'Jax',
+		social: '@jax',
+		quote:
+			'The data insights provided by this tool have been game-changing for our decision-making process. We are now able to make informed decisions quickly.',
 	},
 ]
 
 const testimonials2: Testimonial[] = [
 	{
-		name: 'Michael Brown',
-		title: 'Marketing Director',
+		name: 'Ragatha',
+		social: '@ragatha',
 		quote:
-			'The impact of your product on our marketing strategy has been transformative. Thank you!',
+			"The scalability of this platform is unmatched. It's grown with us every step of the way.",
 	},
 	{
-		name: 'Emily White',
-		title: 'Product Manager',
+		name: 'Gangle',
+		social: '@gangle',
 		quote:
-			'Your product has streamlined our processes and improved our team’s efficiency significantly.',
+			'The user interface is intuitive and clean. Our team was able to get up and running in no time. We are now able to focus on what matters most.',
+	},
+	{
+		name: 'Kinger',
+		social: '@kinger',
+		quote:
+			'The customer support team is top-notch. They are always available to help us when we need it.',
 	},
 ]
 
 const testimonials3: Testimonial[] = [
 	{
-		name: 'David Wilson',
-		title: 'Founder, StartUp XYZ',
+		name: 'Zooble',
+		social: '@zooble',
 		quote:
-			'Your support and product have been critical in our early success. We couldn’t have done it without you!',
+			'The platform has helped us streamline our processes and increase our efficiency. We are now able to focus on what matters most.',
 	},
 	{
-		name: 'Sarah Johnson',
-		title: 'Lead Designer, Design Co.',
+		name: 'Bubble',
+		social: '@bubble',
 		quote:
-			'The innovative features of your product have truly elevated our design workflow.',
+			' Incredible insights. The data provided by this platform has been invaluable to our team.',
+	},
+	{
+		name: 'Gummigoo',
+		social: '@gummigoo',
+		quote:
+			"We've been able to reduce our costs and improve our bottom line since implementing this tool. It's been a game-changer for us.",
 	},
 ]
 
@@ -73,7 +91,7 @@ const TestimonialCard = ({ testimonial }: TestimonialCardProps) => {
 					<Stack gap={0}>
 						<Title order={6}>{testimonial.name}</Title>
 						<Text size="xs" c="gray">
-							{testimonial.title}
+							{testimonial.social}
 						</Text>
 					</Stack>
 				</Group>
@@ -89,21 +107,21 @@ const Testimonials = () => {
 	return (
 		<Section id="testimonials">
 			<Stack gap="xl">
-				<Title ta="center">What our users are saying</Title>
+				<Title ta="center">
+					What people are saying
+					<Text inherit span c="primary" ml="xs">
+						:
+					</Text>
+				</Title>
 				<SimpleGrid
 					cols={{ base: 1, sm: 2, md: 3 }}
 					spacing="xl"
+					verticalSpacing="xl"
 					className="self-center"
 				>
 					{[testimonials1, testimonials2, testimonials3].map(
 						(testimonials, index) => (
-							<Stack
-								key={index}
-								visibleFrom={
-									index === 0 ? undefined : index === 1 ? 'sm' : 'md'
-								}
-								className="max-w-72"
-							>
+							<Stack key={index} className="max-w-80">
 								{testimonials.map((testimonial) => (
 									<TestimonialCard
 										key={testimonial.name}
